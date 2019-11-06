1e980e27c5a7850ecd894cbf4fee0ed49c12280b
Margaret Power <mpowe26@lsu.edu>
2019-11-05 20:03:58 -0600

    configuring for heroku

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
+def post_list(request):
+
+    posts = Post.objects.order_by('published_date')
+
+    return render(request, 'blog/post_list.html', {})
 # Create your views here.
